ffbb30babc50cb398eb4d69a5192b10686cd42a9
Ubeydullah Önder <ubeyd03@outlook.com>
2022-08-20 14:06:02 +0300

    dtypes looking

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+fn main() {
+    let x = 4;
+    let y = [1, 2, 3, 4];
+    let z = &y[0..3];
+    for i in y.iter() {
+        println!("{}", i)
+    }
+    println!("x is {}", x);
+}
