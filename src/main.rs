bbb706e35941e1ae25ab7a379de007a9b056e970
Ubeydullah <ubeyd03@outlook.com>
2022-08-21 12:48:18 +0300

    structers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
+struct User {
+    name: String,
+    username: String,
+    e_mail: String,
+    sign_in: bool,
+    count: i32,
+}
 fn main() {
-    let x = 4;
-    let y = [1, 2, 3, 4];
-    let z = &y[0..3];
-    for i in y.iter() {
-        println!("{}", i)
+    let mut me = User {
+        name: String::from("Ubeydullah"),
+        username: String::from("hikmetli"),
+        e_mail: String::from("hikmetli@gmail.com"),
+        sign_in: true,
+        count: 1,
+    };
+
+    let mut you = build_user(
+        String::from("Ahmet"),
+        String::from("ahmet123"),
+        String::from("ahmet@miail.com"),
+    );
+
+    let mut others = User {
+        name: String::from("Burak"),
+        username: String::from("Burak123"),
+        e_mail: String::from("burak@miail.com"),
+        ..you
+    };
+    println!("{}", others.name);
+}
+
+fn build_user(name: String, username: String, e_mail: String) -> User {
+    User {
+        name,
+        username,
+        e_mail,
+        sign_in: true,
+        count: 1,
     }
-    println!("x is {}", x);
 }
